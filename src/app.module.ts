eb4f829a0df6ca660cfa9da6cbc38e8f387d0685
srnTone <68011813@kmitl.ac.th>
2026-02-25 22:58:44 +0700

    Test git push

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,11 @@
+/* Model 6 Appointment Booking System
+**Core Models**
+
+* Service
+* Appointment
+
+**Description**
+ระบบจองบริการหรือคิว เหมาะสำหรับฝึก validation เชิงเงื่อนไขและการจัดการเวลา*/
 import { Module } from '@nestjs/common';
 
 @Module({
